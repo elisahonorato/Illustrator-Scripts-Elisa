db79a4769e6451dc5f0d54f9c24196b15cac4cdb
Elisa Honorato <elisahonoratolira@uc.cl>
2023-04-11 12:41:47 -0400

    💄 Red Circle Anchors

--- anchors_without_handlers.js
+++ anchors_without_handlers.js
@@ -18,9 +18,19 @@ for (var i = 0; i < all_selectedPaths.length; i++) { // Recorremos los segmentos
   no_handlers = 0;
   for (var j = 0; j < numAnchorPoints; j++) {
     var point = selectedPath.pathPoints[j];
-
     if (point.anchor && point.leftDirection.toString() == point.anchor.toString() && point.rightDirection.toString() == point.anchor.toString()) {
       no_handlers +=1;
+      anchorPoint = point.anchor.toString();
+      var radius = 10;
+      var circle = doc.pathItems.ellipse(point.anchor[1] + radius/2, point.anchor[0] - radius/2, radius, radius);
+      circle.filled = false;
+      circle.strokeWidth = 3;
+      circle.strokeColor = new RGBColor(0xFF0000);
+      circle.strokeColor.red = 255;
+      circle.strokeColor.green = 0;
+      circle.strokeColor.blue = 0;
+      circle.strokeWidth = 3;
+
     }
   }
   alert("Segmentos sin controladores de la selección [" + i + "] :\n" + no_handlers); // Mostramos el número de segmentos del segmento[i]
